0ae3b14f58018dcb829d96d6c79dbc391f53f0fd
DubZero <shumkinalexandrne@gmail.com>
2017-04-03 19:40:11 +0300

    Изменние алгоритмов сглаживанию на сглаживание по Гауссу, изменение алгоритма сглаживанию краев с использованием blend'инга.

--- OpenCV-area-blur/OpenCV-area-blur/OpenCV_area_blur.py
+++ OpenCV-area-blur/OpenCV-area-blur/OpenCV_area_blur.py
@@ -1,49 +1,43 @@
 import numpy as np
 import sys
 import cv2
-image = cv2.imread("D:\\OpenCV\\1.jpg")
+image = cv2.imread("D:\\OpenCV\\2.jpg")
 radius = int(input("Input blur radius in pixels: "))
 
 if radius <= 0:
 	sys.exit()
-def Isincircle(Xc,Yc,r,x,y):
-	if (x - Xc)*(x - Xc) + (y-Yc)*(y-Yc) - r*r <= 0:
-		return True
-	else:
-		return False
-
-# create blur image of edges
-def edgeBlur(image,radius):
-	height, width, channels = image.shape
-	circleCenterX = round(width / 2)
-	circleCenterY = round(height / 2)
-	sub_image = np.ndarray((height,width,3),dtype=np.uint8)
-	for h in range(height):
-		for w in range(width):
-			if (Isincircle(circleCenterX,circleCenterY,round(radius*1.2),h,w) and not(Isincircle(circleCenterX,circleCenterY,round(radius*0.8),h,w))):
-				sub_image[h,w] = image[h,w]	
-	blur_img = cv2.blur(sub_image,(3,3))
-	return blur_img
 
 def CircleBlur(image,radius):
-	blur_image = cv2.blur(image,(15,15))
+	blur_image = cv2.GaussianBlur(image, (7,27), 10)
 	mask = createMask(image,radius)
-	blur_image = cv2.subtract(blur_image,blur_image,blur_image,mask=mask)
-	blur_image = cv2.add(blur_image,image,blur_image,mask = mask)
-	blur_mask = cv2.blur(mask,(15,15))	
-	return blur_image
+	#Smoothing borders
+	cv2.GaussianBlur(mask, (101, 101), 111, dst=mask)
+	res = blendMask(image,blur_image, mask)
+	return res
 
 def createMask(image,radius):
 	height, width, channels = image.shape
 	circleCenterX = round(width / 2)
 	circleCenterY = round(height / 2)
-	mask = np.ndarray((height,width),dtype=np.uint8)
-	for h in range(height):
-		for w in range(width):
-			if(Isincircle(circleCenterX,circleCenterY,radius,h,w)):
-				mask[h,w] = 255
-	
+	mask = np.zeros_like(image)	
+	center = int(circleCenterX), int(circleCenterY)
+	cv2.circle(mask, center, radius, (255, 255, 255), -1)
 	return mask
+
+def blendMask(image1,image2,mask):
+	result = []
+	matrix = np.full_like(mask[:, :, 0], 255)
+	for c in range(0, image1.shape[2]):
+		a = image1[:, :, c]
+		b = image2[:, :, c]
+		m = mask[:, :, c]
+		res = cv2.add(
+			cv2.multiply(b, cv2.divide(matrix - m, 255.0, dtype=cv2.CV_32F), dtype=cv2.CV_8U),
+			cv2.multiply(a, cv2.divide(m, 255.0, dtype=cv2.CV_32F), dtype=cv2.CV_8U),
+			dtype=cv2.CV_8U)
+		result += [res]
+	res = cv2.merge(result)
+	return res
 
 blur = CircleBlur(image,radius)
 
